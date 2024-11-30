6a6a7a526c79179554ab2cfe5b9fe3248ee3db7a
Eleanor Frajka-Williams <eleanorfrajka@gmail.com>
2024-11-30 06:49:48 +0100

    refactor: Remove legacy code not needed for pandoc fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import datetime
-import os
 
 year = datetime.datetime.now(tz=datetime.timezone.utc).date().year
 
@@ -42,29 +41,3 @@ html_static_path = ['_static']
 source_suffix = [".rst", ".md"]
 
 html_logo = "_static/logo.jpg"
-
-# -------------------
-# https://stackoverflow.com/questions/62398231/building-docs-fails-due-to-missing-pandoc
-#from inspect import getsourcefile
-
-# Get path to directory containing this file, conf.py.
-#DOCS_DIRECTORY = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
-
-#def ensure_pandoc_installed(_):
-#    import pypandoc
-
-    # Download pandoc if necessary. If pandoc is already installed and on
-    # the PATH, the installed version will be used. Otherwise, we will
-    # download a copy of pandoc into docs/bin/ and add that to our PATH.
-#    pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
-    # Add dir containing pandoc binary to the PATH environment variable
-#    if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
-#        os.environ["PATH"] += os.pathsep + pandoc_dir
-#    pypandoc.ensure_pandoc_installed(
-#        targetfolder=pandoc_dir,
-#    )
-##        delete_installer=True,
-
-
-#def setup(app):
-#    app.connect("builder-inited", ensure_pandoc_installed)
